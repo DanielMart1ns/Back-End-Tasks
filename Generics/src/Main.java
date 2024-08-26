89ed84696df6ea18b560ef71af328b32495ac0d9
DanielMart1ns <danielmmrodrigues09@gmail.com>
2024-08-26 16:31:58 -0300

    transferring my classes to the List

--- Generics/src/Main.java
+++ Generics/src/Main.java
@@ -1,11 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        //Creating my cars list
+        List<GenericCar> carsList = new ArrayList<>();
+
+        //Creating my attributes
         GenericCar<Long> selectedCar1 = new CustumerCar("Honda", 600, 4);
         GenericCar<Integer> selectedCar2 = new CustumerCar("Hyundai", 800, 3);
         GenericCar<String> selectedCar3 = new CustumerCar("Toyota", 1200, 2);
 
-        selectedCar1.showCustumerCar();
-        selectedCar2.showCustumerCar();
-        selectedCar3.showCustumerCar();
+        //Adding my cars created to the list
+        carsList.add(selectedCar1);
+        carsList.add(selectedCar2);
+        carsList.add(selectedCar3);
+
+        //Showing the results
+        for (GenericCar car : carsList) {
+            car.showCustumerCar();
+        }
     }
 }
