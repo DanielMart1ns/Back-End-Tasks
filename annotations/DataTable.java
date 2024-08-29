abd702666a781e397482b02629c1e51796f12384
DanielMart1ns <danielmmrodrigues09@gmail.com>
2024-08-29 12:43:53 -0300

    adding annotation task

---
+++ annotations/DataTable.java
@@ -0,0 +1,9 @@
+package annotations;
+
+//Applying annotation
+@AnnotationTable(tableName = "userNames")
+public class DataTable {
+    public void showDataTable(){
+        //Table to be created
+    }
+}
