d1d58b74a81ff96fd238509c0591ce7c688ca2ac
DanielMart1ns <danielmmrodrigues09@gmail.com>
2024-10-28 17:04:29 -0300

    chore: including initial settings

---
+++ SpringBootCadastro/src/main/java/br/com/daniel/ModelosHonda/ModelosHondaApplication.java
@@ -0,0 +1,13 @@
+package br.com.daniel.ModelosHonda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelosHondaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelosHondaApplication.class, args);
+	}
+
+}
